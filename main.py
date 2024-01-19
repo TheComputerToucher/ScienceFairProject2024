8cf062021e31a859cf23d84af4c562faab5465e1
TheComputerToucher <damien.brown15@outlook.com>
2024-01-19 13:31:11 -0800

    Fix some stuff

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import matplotlib.pyplot as pyplot
 from mlstuff import *
 from data import *
+import json
 
 DATA_FILE = "./data.csv"
 
 time_steps = 12
+epochs = 20
+
+try:
+    json_file = None
+    with open("config.json", "r") as f:
+        json_file = json.load(f)
+    epochs = int(json_file["epochs"])
+    DATA_FILE = json_file["data_path"]
+except Exception as e:
+    print(f"Failed to load config with error {e}, using defaults of DATA_FILE={DATA_FILE} and epochs={epochs}")
 
 rnn = make_rnn(3, 1, (time_steps, 1), activation=['tanh', 'tanh'])
 
 train_data, test_data, data = get_data(DATA_FILE)
 
-train_model_on_data(rnn, train_data, time_steps, epochs=100)
+train_model_on_data(rnn, train_data, time_steps, epochs)
 
 print("Model successfully trained on CSV file ", DATA_FILE)
 
